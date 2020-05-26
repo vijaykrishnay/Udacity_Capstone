e778dc7178be643ab2bf1d180eb1ad177b68ecb9
Shreyash5432 <kathiriyashreyash@gmail.com>
2020-05-26 10:01:08 -0400

    Defined steps for designing controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -4,11 +4,30 @@ ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
-        # TODO: Implement
+    def __init__(self, accel_limit, brake_deadband, decel_limit, fuel_capacity, max_lat_accel, max_steer_angle, steer_ratio, vehicle_mass, wheel_base, wheel_radius):
+
+        #########################################################################
+
+#         Steps: -
+#             Initialize the yaw controller using the wheel_base, steer_ratio, min_speed, max_lat_accel and max_steer_angle
+#             Initalize the PID controller using Controller gains, minimum and maximum throttle value
+#             Intialize the low pass filter using time constant (tau) and sample time
+
+
+        #########################################################################
+
         pass
 
-    def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
+    def control(self, linear_velocity, angular_velocity, current_velocity, dbw_enabled):
+
+        #########################################################################
+
+#         Use low pass filter to avoid noise in velocity
+#         Use PID controller to get throttle value
+#         Use yaw controller to get steering value
+#         Initialize brake value accoring to the difference between linear_velocity and current_velocity
+#         Return throttle, brake, steer
+
+        #########################################################################
+
         return 1., 0., 0.
