df95256f149a09c7d5858c694bc5d5c526d00944
cdurrans <durranschristopher@gmail.com>
2020-05-30 21:00:13 -0600

    Update tl_classifier
    I got the model to load and now I just need to resize image and return appropriate traffic light. I don't have time for it now though. I'm only pushing to be able to show you tomorrow morning bright and early.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,12 +1,17 @@
 from styx_msgs.msg import TrafficLight
 import keras
+import os
 import numpy as np
+from keras.utils.generic_utils import CustomObjectScope
+
 
 class TLClassifier(object):
     def __init__(self):
         CWD_PATH = os.getcwd()
         PATH_TO_h5 = os.path.join(CWD_PATH, 'light_classification', 'model-best.h5')
-        model = keras.load(PATH_TO_h5)
+        with CustomObjectScope({'relu6': keras.applications.mobilenet.relu6,'DepthwiseConv2D': keras.applications.mobilenet.DepthwiseConv2D}):
+            self.model = keras.models.load_model(PATH_TO_h5)
+#         self.model = keras.models.load_model(PATH_TO_h5)
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,12 +21,11 @@ class TLClassifier(object):
 
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
         """
         #TODO implement light color prediction
         image_expanded = np.expand_dims(image, axis=0)
         print("Image shape: ",image_expanded.shape)
-        prediction = model.predict(image_expanded)
+        prediction = self.model.predict(image_expanded)
         print('Prediction: ',prediction)
 
         return TrafficLight.UNKNOWN
